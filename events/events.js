2526e3edbc4ddc5102d952d52bbade45d3ddbc98
Margarita Privalko <87192791+maggloo@users.noreply.github.com>
2022-09-01 21:16:59 +0300

    Create events.js

---
+++ events/events.js
@@ -0,0 +1,20 @@
+
+<h3>Ссылки</h3>
+ <div><a href="https://en.wikipedia.org/wiki/JavaScript" title="What is javascript?"> Link 1 </a></div>
+ <div><a href="https://learn.javascript.ru/" title="Learn javascript in russian"> Link 2</a></div>
+ <div><a href="https://developer.mozilla.org/en-US/" title="Learn javascript in english"> Link 3 </a></div>
+ 
+<h3>Инпуты</h3>
+<div><input class="first-input" type="text" value="Hello"></div>
+<div><input class="first-input" type="text" value="my name is"></div>
+<div><input class="first-input" type="text" value="Javascript"></div>
+
+<h3>Числа</h3>
+<p>2</p>
+<p>4</p>
+<p>6</p>
+
+<h3>Инпуты-валидация</h3>
+<div><input type="text" data-length="1" placeholder="Введите один символ"></div>
+<div><input type="text" data-length="2" placeholder="Введите два символа"></div>
+<div><input type="text" data-length="3" placeholder="Введите три символа"></div>
